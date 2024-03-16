1faa28ad75bfcd2a12a35c1863e9f2806952cdec
“mateopoquechoque” <mateo.poquechoque@ucb.edu.bo>
2024-03-16 13:30:22 -0400

    se anadio la funcionalidad para sumar el score en caso de que haya un spare

--- src/juego.js
+++ src/juego.js
@@ -1,14 +1,24 @@
 class Juego{
     constructor()
     {
-        this.puntajeAcumulado=0;
+        this.rolls=[];
     }
     roll(pins) {
-       this.puntajeAcumulado+=pins;
+       this.rolls.push(pins);
       }
-    get score()
-    {
-        return this.puntajeAcumulado;
+    get score(){
+       let score=0;
+       let rollIndex=0;
+        for(let frameIndex=0;frameIndex<10;frameIndex++){
+            const frameScore=this.rolls[rollIndex]+this.rolls[rollIndex+1];
+            if(frameScore==10){
+                score+=10+this.rolls[rollIndex+2];
+            }else{
+                score+=frameScore;
+            }
+            rollIndex+=2;
+        }
+        return score;
     }
 }
 
--- src/juegoBolos.test.js
+++ src/juegoBolos.test.js
@@ -1,21 +1,30 @@
 import Juego from "./juego";
-//import juegoTotal from "./juegoTotal";
-let game=new Juego();
+let game;
+beforeEach(()=>{
+  game=new Juego();
+});
 describe("Bolos", () => {
   it("deberia retornar 0 en los 20 juegos", () => {
-    juegoTotal(0,20);
-    expect(game.score).toEqual(0)
+    rollMany(0,20);
+    expect(game.score).toEqual(0);
   });
   it("deberia retornar 20 si derribo 1 pino por cada tiro", () => {
-    juegoTotal(1,20);
-    expect(game.score).toEqual(20)
+    rollMany(1,20);
+    expect(game.score).toEqual(20);
+  });
+  it("deberia retornar el resultado correcto ", () => {
+    game.roll(5);
+    game.roll(5);
+    game.roll(3);
+    rollMany(0,17);
+    expect(game.score).toEqual(16);
   });
 });
 
-function juegoTotal(pinos, tiros)
+function rollMany(pins, rolls)
 {
-    for(let i=0;i<tiros;i++)
+    for(let i=0;i<rolls;i++)
     {
-         game.roll(pinos);
+         game.roll(pins);
     }
 }
